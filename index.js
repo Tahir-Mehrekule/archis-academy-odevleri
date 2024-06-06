14774e8a4cd35b15285e420655fd39584cf2ecf0
Tahir-Mehrekule <nace4121@gmail.com>
2024-06-06 14:10:26 +0300

    ilk gönderi

---
+++ index.js
@@ -0,0 +1,46 @@
+//görev 1.1 ve 1.2  
+
+let firstname = "Tahir";
+let lastname = "mehrekule";
+
+let message = `merhaba ${firstname} ${lastname} `;
+console.log(message);
+
+
+const sayı1 = 50;
+const sayı2 = 20;
+const result = sayı1 + sayı2;
+console.log(result);
+
+//görev 2 aslında görev 1 de göstermiş oldum
+
+//görev 3,1
+
+let sentence = "merhaba benim adım tahir";
+
+let CharacterCount = sentence.length;
+
+console.log('cümlede ki karakter sayısı;',CharacterCount);
+
+//görev 3,2 
+let sentence1 = "MERHABA BENİM ADIM TAHİR";
+
+let lowerCaseSentence = sentence1.toLowerCase();
+
+console.log('cümlede ki kücük haftler;',lowerCaseSentence);
+
+//görev 4.1
+
+let cümle = 'İle\'nin Yazılışı';
+
+console.log(cümle);
+
+//görev 4.2 
+
+let firstname1 = "tahir";
+let surname = "mehrekule";
+let age = 21;
+let hobby = "video game";
+let introduction = `merhaba benim adım ${firstname1} soyadım ${surname} yaşım ${age} hobilerimden biriyse ${hobby} `;
+
+console.log(introduction);
